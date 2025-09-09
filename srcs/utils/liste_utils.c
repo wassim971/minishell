94a37328bbfb69a80f13300fefb3b45fe3b68522
Vavongg <alexis.itvg@gmail.com>
2025-09-09 17:20:35 +0200

    fait

--- srcs/utils/liste_utils.c
+++ srcs/utils/liste_utils.c
@@ -14,8 +14,8 @@
 
 static bool	fill_cmd(t_data *data, t_token *tmp)
 {
-	if (!get_infile(data, tmp, data->cmd->prev) && data->cmd->prev->infile !=
-		-1)
+	if (!get_infile(data, tmp, data->cmd->prev)
+		&& data->cmd->prev->infile != -1)
 		return (false);
 	if (data->cmd->prev->infile == -1)
 	{
@@ -23,8 +23,8 @@ static bool	fill_cmd(t_data *data, t_token *tmp)
 		data->cmd->prev->outfile = -1;
 		return (true);
 	}
-	if (!get_outfile(tmp, data->cmd->prev, data) && data->cmd->prev->outfile !=
-		-1)
+	if (!get_outfile(tmp, data->cmd->prev, data)
+		&& data->cmd->prev->outfile != -1)
 		return (false);
 	if (data->cmd->prev->outfile == -1)
 	{
